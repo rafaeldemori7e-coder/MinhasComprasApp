22ef3badc16c9819d005f9ca25756ed7bc1b01a3
rafaeldemori7e-coder <rafaeldemori.7e@gmail.com>
2026-03-15 19:16:52 -0300

    .

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using MauiAppMinhasCompras.Helpers;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
         static SQLiteDatabaseHelper _db;
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
 
         public static SQLiteDatabaseHelper Db
         {
@@ -14,8 +19,8 @@ namespace MauiAppMinhasCompras
                 if (_db == null)
                 {
                     string path = Path.Combine(
-                        Environment.GetFolderPath(
-                            Environment.SpecialFolder.LocalApplicationData),"banco_sqlite_compras.db3");
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
 
                     _db = new SQLiteDatabaseHelper(path);
                 }
