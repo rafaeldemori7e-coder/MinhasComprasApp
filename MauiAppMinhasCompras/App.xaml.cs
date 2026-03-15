748120eb0a0c77ff2a52c8fd4808db77ed2ad3ab
rafaeldemori7e-coder <rafaeldemori.7e@gmail.com>
2026-03-14 23:08:55 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MauiAppMinhasCompras.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),"banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Views/ListaProduto.cs
@@ -0,0 +1,15 @@
+namespace MauiAppMinhasCompras.Views;
+
+public class ListaProduto : ContentPage
+{
+	public ListaProduto()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
